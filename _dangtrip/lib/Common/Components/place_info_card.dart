38a835dde8749c7e91e063b1be9f93d276bb45a3
yongjunhan <hanyongjun.dev@gmail.com>
2023-04-19 20:54:24 +0900

    placeCard

--- _dangtrip/lib/Common/Components/place_info_card.dart
+++ _dangtrip/lib/Common/Components/place_info_card.dart
@@ -1,14 +1,130 @@
+// import 'dart:convert';
+
+import 'package:_dangtrip/Common/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class PlaceInfoCard extends StatelessWidget {
-  const PlaceInfoCard({super.key});
+  // 썸네일
+  final Widget image;
+  //상호명
+  final String placeName;
+  //견종
+  final String dogType;
+  //태그
+  final List<String> tags;
+  //평점
+  final double rating;
+
+  const PlaceInfoCard({
+    required this.image,
+    required this.placeName,
+    required this.tags,
+    required this.rating,
+    required this.dogType,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('여행루트짜기'),
+    return SafeArea(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: image,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      placeName,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          size: 16,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(rating.toString()),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      tags.join(' · '),
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: PRIMARY_COLOR,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class DogType extends StatelessWidget {
+  final String type;
+
+  const DogType({
+    required this.type,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          'lib/assets/logo/dogType.png',
+          fit: BoxFit.cover,
+          scale: 2,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Text(
+          type,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+        )
+      ],
     );
   }
 }
--- _dangtrip/lib/screens/home.dart
+++ _dangtrip/lib/screens/home.dart
@@ -86,7 +86,60 @@ class _HomeScreenState extends State<HomeScreen>
                 child: const Text('주변'),
               ),
             ),
-            const PlaceInfoCard(),
+            Center(
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 16, right: 16),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xffe1e1e1)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.search_rounded,
+                                color: Color(0xffe1e1e1),
+                                size: 24,
+                              ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    '어디로 떠나시나요 ?',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xffe1e1e1),
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      PlaceInfoCard(
+                        image: Image.asset(
+                          'lib/assets/event/kanu_dog.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                        placeName: '카누하개',
+                        tags: const ['반려견 동반', '무료주차', 'wifi'],
+                        rating: 4.7,
+                        dogType: '소형견',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Center(
               child: Container(
                 child: const Text('댕생네컷'),
